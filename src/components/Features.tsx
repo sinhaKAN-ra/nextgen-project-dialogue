7220a137269fa9d97e97bc4972bb096acf6556d4
sinhaKAN-ra <sinhakaran7701@gmail.com>
2025-04-24 01:09:17 +0530

    slight fix

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -128,7 +128,7 @@ const Features = () => {
     <motion.section 
       id="features" 
       initial={{ opacity: 0, y: 50 }} 
-      whileInView={{ opacity: .6, y: 0 }} 
+      whileInView={{ opacity: 1, y: 0 }} 
       viewport={{ once: true, amount: 0.3 }} 
       transition={{ duration: 0.8 }} 
       className="py-28 relative overflow-hidden"
